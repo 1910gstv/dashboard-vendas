5b1bc3fc81d7432b31716c4e5ba976f5176fa4f9
Gustavo <gustavo.hpaula@outlook.com>
2024-03-06 23:57:28 -0300

    implementando a volta para homepage

--- src/ajax.js
+++ src/ajax.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
-    $("#cadastrarVenda").on("click", () => {
-  
-  
-      $.get("cadastrarVenda.html", (data) => {
-          $("#homeView").html(data)
-      })
-    })
+  $("#cadastrarVenda").on("click", () => {
+    $.get("cadastrarVenda.html", (data) => {
+      $("#homeView").html(data);
+    });
   });
+
+  $("#homeLink").on("click", () => {
+    location.reload(); //Implementar outra funcionalidade sem precisar recarregar a página
+  });
+});
